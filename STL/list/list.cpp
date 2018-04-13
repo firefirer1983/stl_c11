2debe21c2f1899c36942ec0802cceec7c8d5b655
fyman.zhang <fyman.zhang@icloud.com>
2018-04-13 17:27:17 +0800

    add list

--- STL/list/list.cpp
+++ STL/list/list.cpp
@@ -1,14 +1,7 @@
 #include <gtest/gtest.h>
-#include <string>
-#include <vector>
-#include <ostream>
-#include <array>
+#include <list>
 
-using std::vector;
-using std::string;
-using std::cout;
-using std::endl;
-using std::array;
+using namespace std;
 
 class GTest : public ::testing::Test {
 
@@ -19,13 +12,41 @@ protected:
   virtual void TearDown(){
   }
 };
-
-TEST_F(GTest, Init_GTest){
-  const int ARRAY_SIZE = 100;
-  array<int, ARRAY_SIZE> array_test = {0,1,2,3};
-  for(int i=0; i< ARRAY_SIZE; i++) {
-    cout << "array_test[" << i << "]=" << array_test[i] << endl;
+template <typename T>
+void print_l(const list<T> &l) {
+  for(auto it=l.begin(); it!=l.end(); it++) {
+    cout << *it << " ";
   }
+  cout << endl;
+}
+TEST_F(GTest, Init_GTest){
+  list<int> list_int;
+  for (int i = 0; i < 10; i++) {
+    list_int.push_back(i);
+  }
+  cout<<"init list with 0~9: " << endl;
+  print_l(list_int);
+  cout << endl;
+  list<int> list_copy;
+  list_copy.assign(list_int.begin(), list_int.end());
+  cout << "list_copy:" << endl;
+  print_l(list_copy);
+
+  cout << "Remove 5 in list_copy" << endl;
+  list_copy.remove(5);
+  print_l(list_copy);
+  cout << "size: " << list_copy.size() << endl;
+
+  cout << "Erase element at position 5: " << endl;
+  list<int>::iterator it = list_copy.begin();
+  for(int i=0; i < 5; i++) {
+    it ++;
+  }
+  list_copy.erase(it);
+  print_l(list_copy);
+
+  bool test = false;
+  list_copy.remove_if(test);
 }
 int main(int argc, char *argv[])
 {
