d4337f8a8f6857073d5735f336e8738bb4dd9741
fyman.zhang <fyman.zhang@icloud.com>
2018-04-20 22:29:52 +0800

    add unordered_map

--- STL/unordered_map/unordered_map.cpp
+++ STL/unordered_map/unordered_map.cpp
@@ -3,12 +3,16 @@
 #include <vector>
 #include <ostream>
 #include <array>
+#include <unordered_map>
+#include <functional>
 
 using std::vector;
 using std::string;
 using std::cout;
 using std::endl;
 using std::array;
+using std::unordered_map;
+using std::pair;
 
 class GTest : public ::testing::Test {
 
@@ -21,11 +25,32 @@ protected:
 };
 
 TEST_F(GTest, Init_GTest){
-  const int ARRAY_SIZE = 100;
-  array<int, ARRAY_SIZE> array_test = {0,1,2,3};
-  for(int i=0; i< ARRAY_SIZE; i++) {
-    cout << "array_test[" << i << "]=" << array_test[i] << endl;
+  unordered_map<string, string> color_map;
+  pair< unordered_map<string,string>::iterator , bool> ret =  color_map.emplace("RED", "#FF0000");
+  if(!ret.second) {
+    cout << "insert " << ret.first->first <<  " failed!" << endl;
   }
+  ret = color_map.emplace("GREEN", "#00FF00");
+  if(!ret.second) {
+    cout << "insert " << ret.first->first <<  " failed" << endl;
+  }
+  ret = color_map.emplace("BLUE", "#0000FF");
+  if(!ret.second) {
+    cout << "insert " << ret.first->first <<  " failed" << endl;
+  }
+  ret = color_map.emplace("RED", "#0000FF");
+
+  if(!ret.second) {
+    cout << "insert " << ret.first->first <<  " failed" << endl;
+  }
+  unordered_map<string,string>::iterator it = color_map.find("GREEN");
+  if(it != color_map.end()) {
+    cout << "find GREEN success: " << it->second << endl;
+  }
+  cout << "RED BUCKET: " << color_map.bucket("GREEN") << endl;
+  cout << "GREEN BUCKET: " << color_map.bucket("GREEN") << endl;
+  cout << "BLUE BUCKET: " << color_map.bucket("GREEN") << endl;
+
 }
 int main(int argc, char *argv[])
 {
