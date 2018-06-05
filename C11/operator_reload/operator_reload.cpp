adcd81b5e83438baf83585092a1beefc1856b5ee
firefirer1983 <fyman.zhang@gmail.com>
2018-06-06 00:25:05 +0800

    update

--- C11/operator_reload/operator_reload.cpp
+++ C11/operator_reload/operator_reload.cpp
@@ -69,88 +69,60 @@ TEST_F(GTest, WEAK_PTR_GTest) {
 
 }
 
-TEST_F(GTest, NetName_GTest) {
-	const char divChar = '-';
-	const char *netPostfix = "net";
-	const char *usbBin = "ZY-3A200.bin";
-	const char *netBin = "ZY-3A200-net.bin";
-	char baseName[128];
-	char fullName[128];
-	memset(baseName, 0, sizeof(baseName));
-	memset(fullName, 0, sizeof(fullName));
-	strncpy(baseName, usbBin, strlen(usbBin) - strlen(".bin"));
-
-	snprintf(fullName, sizeof(fullName), "%s%c%s.bin", baseName, divChar, netPostfix);
-	printf("%s\n", fullName);
-	EXPECT_STREQ(string(fullName).c_str(), string(netBin).c_str());
-}
-
-TEST_F(GTest, Strstr_GTest) {
-	const char divChar = '-';
-	const char *netPostfix = "net";
-	const char *usbBin = "ZY-3A200.bin";
-	const char *netBin = "ZY-3A200-net.bin";
-	char baseName[128];
-	memset(baseName, 0, sizeof(baseName));
-
-	EXPECT_NE(strstr(netBin, baseName), nullptr);
-}
-
-
-class Vector {
-public:
-	// 默认构造函数
-	Vector(std::initializer_list<double> lst) :
-			sz_(lst.size()),
-			ary_(new double[lst.size()]) {
-		std::copy(lst.begin(), lst.end(), ary_);
-		printf("Vector default constructor\n");
-	}
-
-	~Vector() {
-		printf("Vector destructor\n");
-		delete[] ary_;
-		sz_ = 0;
-	}
-
-	// 拷贝构造函数
-	Vector(const Vector &v) :
-			sz_(v.sz_),
-			ary_(new double[v.sz_]) {
-		printf("Vector copy constructor\n");
-		std::copy(v.ary_, v.ary_ + sz_, ary_);
-	}
-
-	Vector(Vector &&v) {
-		printf("Vector move constructor\n");
-
-	}
-
-	// 拷贝赋值运算符
-	Vector &operator=(const Vector &v) {
-		printf("Vector operator=\n");
-		delete[] ary_;
-		sz_ = v.sz_;
-		ary_ = new double[v.sz_];
-		std::copy(v.ary_, v.ary_ + sz_, ary_);
-		return *this;
-	}
-
-	size_t size() {
-		return sz_;
-	}
-
-	void show() {
-		for (int i = 0; i != static_cast<int>(sz_); i++) {
-			printf("%f ", ary_[i]);
-		}
-		printf("\n");
-	}
-
-private:
-	double *ary_;
-	size_t sz_;
-};
+//class Vector {
+//public:
+//	// 默认构造函数
+//	Vector(std::initializer_list<double> lst) :
+//			sz_(lst.size()),
+//			ary_(new double[lst.size()]) {
+//		std::copy(lst.begin(), lst.end(), ary_);
+//		printf("Vector default constructor\n");
+//	}
+//
+//	~Vector() {
+//		printf("Vector destructor\n");
+//		delete[] ary_;
+//		sz_ = 0;
+//	}
+//
+//	// 拷贝构造函数
+//	Vector(const Vector &v) :
+//			sz_(v.sz_),
+//			ary_(new double[v.sz_]) {
+//		printf("Vector copy constructor\n");
+//		std::copy(v.ary_, v.ary_ + sz_, ary_);
+//	}
+//
+//	Vector(Vector &&v) {
+//		printf("Vector move constructor\n");
+//
+//	}
+//
+//	// 拷贝赋值运算符
+//	Vector &operator=(const Vector &v) {
+//		printf("Vector operator=\n");
+//		delete[] ary_;
+//		sz_ = v.sz_;
+//		ary_ = new double[v.sz_];
+//		std::copy(v.ary_, v.ary_ + sz_, ary_);
+//		return *this;
+//	}
+//
+//	size_t size() {
+//		return sz_;
+//	}
+//
+//	void show() {
+//		for (int i = 0; i != static_cast<int>(sz_); i++) {
+//			printf("%f ", ary_[i]);
+//		}
+//		printf("\n");
+//	}
+//
+//private:
+//	double *ary_;
+//	size_t sz_;
+//};
 
 template<typename T>
 class Vector {
@@ -158,26 +130,34 @@ public:
 	Vector(const std::initializer_list<T> &lst) :
 			sz_(lst.size()),
 			ary_(new T[lst.size()]) {
-		T *tmp = ary_;
-		for (auto &m:lst) {
-			*(tmp++) = m;
-		}
+
+		std::copy(lst.begin(), lst.end(), ary_);
 	}
 
-	Vector(const Vector &v) :
+	Vector(const Vector& v) :
 			sz_(v.sz_),
 			ary_(new T[v.sz_]) {
-		T *tmp = ary_;
-		for (const T &m:v) {
-			*(tmp++) = m;
-		}
+		std::copy(v.begin(), v.end(), ary_);
 	}
 
-	T *begin() {
+	Vector& operator=(const Vector &v) {
+		if(&v != this)
+			std::copy(v.begin(), v.end(), ary_);
+		return *this;
+	}
+
+	void show() {
+		for(unsigned i=0; i!=sz_; i++) {
+			printf("%f ", ary_[i]);
+		}
+		printf("\n");
+	}
+
+	T *begin() const {
 		return ary_;
 	}
 
-	T *end() {
+	T *end() const {
 		return (ary_ + sz_);
 	}
 
@@ -187,16 +167,16 @@ public:
 	}
 
 private:
-	T *ary_;
 	size_t sz_;
+	T *ary_;
 };
 
 TEST_F(GTest, Vector_GTest) {
-	unique_ptr<Vector> pv;
+	unique_ptr<Vector<float>> pv;
 	{
 		try {
-			Vector v1({0, 1, 2, 3, 4, 5});
-			pv.reset(new Vector(v1));
+			Vector<float> v1({0, 1, 2, 3, 4, 5});
+			pv.reset(new Vector<float>(v1));
 			v1.show();
 
 		} catch (std::bad_alloc) {
@@ -204,9 +184,28 @@ TEST_F(GTest, Vector_GTest) {
 		}
 	}
 	pv->show();
-	Vector Va({10, 9, 8, 7, 6, 5});
-	Vector Vb({0, 1, 2, 3, 4, 5});
+	Vector<float> Va({10, 9, 8, 7, 6, 5});
+	Vector<float> Vb({0, 1, 2, 3, 4, 5});
 	(Vb = Va).show();
+}
+
+template <typename T>
+class LessThan{
+public:
+	LessThan(const T &v) :
+			val(v){
+	}
+	bool operator()(const T &c) const {
+		return (c < val);
+	}
+
+private:
+	const T val;
+};
+TEST_F(GTest, LessThan_GTest) {
+	LessThan<int> lessThan(5);
+  EXPECT_TRUE(lessThan(4));
+	EXPECT_FALSE(lessThan(6));
 }
 
 int main(int argc, char *argv[]) {
